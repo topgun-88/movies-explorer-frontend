86dfab326992efae69020b1156d47a319eb44739
Dmitry Kaldarbekov <topgun88@yandex.ru>
2021-11-18 00:54:58 +0300

    level-2 rev.1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,8 +7,8 @@ const Profile = () => {
         <form className='profile'>
             <h2 className='profile__title'>Привет, Виталий</h2>
             <div className='profile__input-block'>
-                <label className='profile__label'>Имя<input className='profile__input' type='text'></input></label>
-                <label className='profile__label'>E-mail<input className='profile__input' type='text'></input></label>
+                <label className='profile__label'><span className='profile__label-span'>Имя</span><input className='profile__input' type='text'></input></label>
+                <label className='profile__label'><span className='profile__label-span'>E-mail</span><input className='profile__input' type='text'></input></label>
             </div>
             <button className='profile__submit'>Редактировать</button>
             <button className='profile__signout-btn' type='button'>Выйти из аккаунта</button>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -9,7 +9,7 @@ const SearchForm = ({isShorts, setIsShorts}) => {
             <div className='search-form__search-field'>
                 <label className='search-form__label'>
                     <svg className='search-form__label-ico' width="34" height="34" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M18.793 18.264a3.667 3.667 0 1 1-5.186-5.186 3.667 3.667 0 0 1 5.186 5.186Zm.44 1.383a5.001 5.001 0 1 1 .943-.943l3.567 3.567-.943.942-3.567-3.566Z" fill="#959595"/></svg>
-                    <input className='search-form__input-text' type='text' value={findValue} onChange={(e)=>setFindValue(e.target.value)} placeholder='Фильм'/>
+                    <input className='search-form__input-text' type='text' value={findValue} onChange={(e)=>setFindValue(e.target.value)} required placeholder='Фильм'/>
                 </label>
                 <button className='search-form__submit'>
                     <svg width="34" height="34" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M18.793 18.264a3.667 3.667 0 1 1-5.186-5.186 3.667 3.667 0 0 1 5.186 5.186Zm.44 1.383a5.001 5.001 0 1 1 .943-.943l3.567 3.567-.943.942-3.567-3.566Z" fill="#fff"/></svg>
